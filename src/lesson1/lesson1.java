f51f1dd11965a1c6db1875dc3517b3c36f7da445
panterpower <lider.svb@gmail.com>
2022-06-05 21:19:00 +0800

    Всё остальное

--- src/lesson1/lesson1.java
+++ src/lesson1/lesson1.java
@@ -5,21 +5,63 @@ public class lesson1 {
     public static void main(String[] args) {
 
 //        int myNumber = 5;
-//        System.out.print(myNumber);
+//        System.out.println(myNumber);
 
 //        byte myByte1 = -128;
 //        byte myByte2 = 127;
-//        System.out.print(myByte1);
-//        System.out.print(myByte2);
+//        System.out.println(myByte1);
+//        System.out.println(myByte2);
 
 //        short myShort1 = -32000;
 //        short myShort2 = 32000;
-//        System.out.print(myShort1);
-//        System.out.print(myShort2);
+//        System.out.println(myShort1);
+//        System.out.println(myShort2);
 
-        int myNumber1 = -2_000_000_000;
-        int myNumber2 = 2_000_000_000;
-        System.out.print(myNumber1);
-        System.out.print(myNumber2);
+//        int myInteger1 = -2_000_000_000;
+//        int myInteger2 = 2_000_000_000;
+//        System.out.println(myInteger1);
+//        System.out.println(myInteger2);
+
+//        long myLong1 = -2_000_000_000L;
+//        long myLong2 = 2_000_000_000L;
+//        System.out.println(myLong1);
+//        System.out.println(myLong2);
+
+//        float myFloat = 3.123456789f;
+//        double myDouble = 3.123456789123456789;
+//        System.out.println(myFloat);
+//        System.out.println(myDouble);
+//
+//        boolean byBoolean1 = true;
+//        boolean byBoolean2 = false;
+//
+//        char myChar = 'a';
+//        System.out.println(myChar);
+//        System.out.println(myChar + 1);
+
+        int myInteger1 = 100;
+        int myInteger2 = myInteger1;
+
+        myInteger1 = 101;
+        System.out.println(myInteger1);
+        System.out.println(myInteger2);
+
+//  [1,2,3,4]
+         char[] myArray = new char[6];
+         myArray[2] = 'с';
+         myArray[4] = 'о';
+         myArray[5] = 'к';
+         myArray[1] = 'е';
+         myArray[0] = 'ч';
+         myArray[3] = 'н';
+        System.out.println(myArray);
+
+        char[] myArray2 = myArray;
+        myArray[0] = 'к';
+        System.out.println(myArray2);
+
+        myArray2[0] = 'ч';
+        System.out.println(myArray);
+
     }
 }
