2429aa89563a9d46bd5451f85587815f930f2e97
panterpower <lider.svb@gmail.com>
2022-06-05 20:36:06 +0800

    урок1 byte short int

---
+++ src/lesson1/lesson1.java
@@ -0,0 +1,25 @@
+package lesson1;
+
+public class lesson1 {
+
+    public static void main(String[] args) {
+
+//        int myNumber = 5;
+//        System.out.print(myNumber);
+
+//        byte myByte1 = -128;
+//        byte myByte2 = 127;
+//        System.out.print(myByte1);
+//        System.out.print(myByte2);
+
+//        short myShort1 = -32000;
+//        short myShort2 = 32000;
+//        System.out.print(myShort1);
+//        System.out.print(myShort2);
+
+        int myNumber1 = -2_000_000_000;
+        int myNumber2 = 2_000_000_000;
+        System.out.print(myNumber1);
+        System.out.print(myNumber2);
+    }
+}
