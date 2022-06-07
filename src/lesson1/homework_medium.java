b01fa49a55096a99cd340f2dbe7f8ca379315b44
panterpower <lider.svb@gmail.com>
2022-06-07 21:10:53 +0800

    Средний уровень Оставшиеся 2 задачи Всё сделал

--- src/lesson1/homework_medium.java
+++ src/lesson1/homework_medium.java
@@ -1,5 +1,7 @@
 package lesson1;
 
+import java.util.Arrays;
+
 public class homework_medium {
     public static <string> void main(String[] args) {
 
@@ -31,81 +33,55 @@ public class homework_medium {
         // Создать массив чисел, в котором будут: все числа из этих двух массивов,
         // и результат умножения чисел с одинаковым порядковым номером
         //
+
+
+        int[] myArray1 = new int[5];
+        myArray1[0] = 1;
+        myArray1[1] = 2;
+        myArray1[2] = 5;
+        myArray1[3] = 7;
+        myArray1[4] = 10;
+
+        int[] myArray2 = new int[5];
+        myArray2[0] = 2;
+        myArray2[1] = 3;
+        myArray2[2] = 2;
+        myArray2[3] = 17;
+        myArray2[4] = 15;
+
+        // Здесь знаний не хватает и сделал тупо, но эффективно
+        int[] myArrayConglutination = new int[15];
+        myArrayConglutination[0] = myArray1[0];
+        myArrayConglutination[1] = myArray1[1];
+        myArrayConglutination[2] = myArray1[2];
+        myArrayConglutination[3] = myArray1[3];
+        myArrayConglutination[4] = myArray1[4];
+        myArrayConglutination[5] = myArray2[0];
+        myArrayConglutination[6] = myArray2[1];
+        myArrayConglutination[7] = myArray2[2];
+        myArrayConglutination[8] = myArray2[3];
+        myArrayConglutination[9] = myArray2[4];
+        myArrayConglutination[10] = myArray1[0] * myArray2[0];
+        myArrayConglutination[11] = myArray1[1] * myArray2[1];
+        myArrayConglutination[12] = myArray1[2] * myArray2[2];
+        myArrayConglutination[13] = myArray1[3] * myArray2[3];
+        myArrayConglutination[14] = myArray1[4] * myArray2[4];
+
+        System.out.println(Arrays.toString(myArrayConglutination));
+
         //Ожидаемый результат:
         //1,2,5,7,10,2,3,2,17,15,2,6,10,119,150
         //(первый массив - 1,2,5,7,10), (второй массив - 2,3,2,17,15),
         //(результат перемножения - (1*2), (2*3), (5*2), (7*17), (10*15)
 
-        сhar[] myArray1 = new char[5];
-//        сhar[] myArray2 = new char[5];
-
-        myArray1[0] = '1';
-        myArray1[1] = '2';
-        myArray1[2] = '5';
-        myArray1[3] = '7';
-        myArray1[4] = "10";
-
-//        myArray2[0] = '2';
-//        myArray2[1] = '3';
-//        myArray2[2] = '2';
-//        myArray2[3] = '17';
-//        myArray2[4] = '15';
-
-
-        System.out.println(myArray1);
-
-
-//        myArray[3] = 'h';
-//        System.out.println(myArray);
-
         //Задача №4
         //В слове "Hello world!" заменить все l на r, сделать все буквы заглавными, выбрать первые 8 символов
+        String wordHello = "Hello world!";
+        String replaced = wordHello.replaceAll("l", "r");
+        String big = replaced.toUpperCase();
+        String firstEight = big.substring(0,8);
 
-
-
-//        //Базовый уровень
-//        //Задача №1
-//        //Дано (их менять нельзя)
-//        String hi = "                Hello ";
-//        String world = " WoRld!";
-//        char newLine = '\n';
-//
-//        //Создать из трех переменных единую строку,
-//
-//        String myString = hi + world + newLine;
-//        System.out.println(myString);
-//
-//        //Привести к правильному виду (Ниже)
-//        //затроить (Можно вызвать тольку одну команду System.out.print())
-//
-//        System.out.print((hi.trim() + world.toLowerCase() + newLine).repeat(3));
-//
-//        //Результат вывода на экран:
-//        //Hello world!
-//        //Hello world!
-//        //Hello world!
-//
-//
-//        //Задача №2
-//        //Создать переменные с ростом, весом.
-//        float growth = 1.91f;
-//        float weight = 100f;
-//
-//        //Произвести расчет индекса массы тела (вес) / (рост * рост) используя переменные, вывести на экран
-//
-//        float index = weight / (growth * growth);
-//        String result = String.format("%.1f",index);
-//
-//        //Пример результата вывода на экран:
-//        System.out.println(result);
-//
-//        //21.0
-//
-//        //Задача №3
-//        //Создать из массива букв a,b,c,d,e, строку,вывести на экран , поменять в массиве 4 букву по счету на h,
-//        //и снова создать строку, вывести на экран
-//
-
+        System.out.println(firstEight);
 
     }
 
