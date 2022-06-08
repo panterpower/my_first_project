5ccd3bc5c9a2a01b604fe1437b0475e4998e79fb
panterpower <lider.svb@gmail.com>
2022-06-09 02:31:12 +0800

    Урок2

---
+++ src/lesson2/lesson2.java
@@ -0,0 +1,81 @@
+package lesson2;
+
+public class lesson2 {
+    public static void main(String[] args) {
+//        int number1 = 11;
+//        int number2 = 12;
+//
+//        if (number1 == number2){
+//            System.out.println("Два числа равны");
+//        }else {
+//            System.out.println("Два числа не равны");
+//        }
+//
+//        int number3 = 0;
+//
+//        int number4 = 4;
+//        if (number3 > 0) {
+//            System.out.println("Число пололжительное");
+//        } else if (number3 == 0) {
+//            System.out.println(number4);
+//            String text = "Какой-то текст";
+//            System.out.println(text);
+//            System.out.println("Число ноль");
+//        }
+//
+//        else {
+//            System.out.println("Число отрицательное");
+//        }
+//
+//        String name1 = "Yuri";
+//        String name2 = "Yuro";
+//        if (name1.equals(name2)){
+//            System.out.println("Равны");
+//        } else {
+//            System.out.println("Не равны");
+//        }
+//        // Логическое И (&&)
+//        //true && true == true
+//        //false && true == false
+//        //false && false == false
+//
+//        int number5 = 11;
+//        int number6 = 12;
+//        if ( (number5 == number6) && (name1.equals(name2))) {
+//            System.out.println("Числа равны и строки равны");
+//        }
+//
+//        // Логическое ИЛИ (||)
+//        //true || false == true
+//        //true || true == true
+//        //false || true == true
+//        //false || false == false
+//
+//        if ( (number5 == number6) || (name1.equals(name2))) {
+//            System.out.println("Логическое ИЛИ сработало");
+//        }
+//
+//        // Логическое НЕ (!)
+//
+//        int[] numbers = new int[3];
+//        numbers[0] = 2;
+//        numbers[1] = 3;
+//        numbers[2] = 4;
+//
+//        for (int num :numbers) {
+//            System.out.println(num);
+//        }
+//
+//        for (int i = 0; i < 3; i++) {
+//            System.out.println(numbers[i]);
+//
+//        }
+
+        int index = 0;
+        while (index < 10) {
+            System.out.println("Привет мир");
+            index +=1;
+        }
+
+    }
+}
